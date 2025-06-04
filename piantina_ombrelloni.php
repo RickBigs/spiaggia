79ff52d4d48fd20f86227e444f834a0322808f41
Riccardo Grossi <riccardogrossi12@gmail.com>
2025-06-04 15:42:33 +0200

    features

--- piantina_ombrelloni.php
+++ piantina_ombrelloni.php
@@ -30,27 +30,96 @@ while ($row = $result->fetch_assoc()) {
 </head>
 <body>
     <h2>Piantina Ombrelloni - Data: <?php echo htmlspecialchars($data); ?></h2>
-    <form method="get">
-        <label>Seleziona data: <input style="width: 120px"type="date" name="data" value="<?php echo htmlspecialchars($data); ?>"></label>
-        <button type="submit">Cerca</button>
-    </form>
-    <div style="margin-top:30px;">
-        <?php foreach ($ombrelloni as $omb): 
-            $prenotato = !empty($omb['id_prenotazioni']);
-            $class = $prenotato ? 'prenotato' : 'libero';
-        ?>
-        <div class="ombrellone <?php echo $class; ?>" onclick="modificaOmbrellone(<?php echo $omb['id_ombrellone']; ?>)">
-            <?php echo $omb['n_ombrellone']; ?><br><small><?php echo $omb['fila']; ?></small>
-            <?php if ($prenotato): ?>
-                <div class="tooltip">
-                    Prenotato da:<br>
-                    <strong><?php echo htmlspecialchars($omb['nome'] . ' ' . $omb['cognome']); ?></strong><br>
-                    Dal: <?php echo htmlspecialchars($omb['data_inizio']); ?><br>
-                    Al: <?php echo htmlspecialchars($omb['data_fine']); ?>
-                </div>
-            <?php endif; ?>
+
+<form method="get">
+<label>Seleziona data:</label> 
+<input style="width: 120px"type="date" name="data" value="<?php echo htmlspecialchars($data); ?>"></label>
+    <button type="submit">Cerca</button>
+    
+        <form method="get" style="display: inline;">
+            <input type="hidden" name="data" value="<?php echo date('Y-m-d', strtotime($data.' -1 day')); ?>">
+            <button type="submit">← Giorno precedente</button>
+        </form>
+        <form method="get" style="display: inline;">
+            <input type="hidden" name="data" value="<?php echo date('Y-m-d', strtotime($data.' +1 day')); ?>">
+            <button type="submit">Giorno successivo →</button>
+        </form>      
+</form>
+
+    <div class="piantina-container">
+        <div class="fila fila-4">
+            <?php for ($i = 10; $i >= 8; $i--): $omb = $ombrelloni[10-$i];
+                $prenotato = !empty($omb['id_prenotazioni']);
+                $class = $prenotato ? 'prenotato' : 'libero';
+            ?>
+            <div class="ombrellone <?php echo $class; ?>" onclick="modificaOmbrellone(<?php echo $omb['id_ombrellone']; ?>)">
+                <?php echo $omb['n_ombrellone']; ?><br><small><?php echo $omb['fila']; ?></small>
+                <?php if ($prenotato): ?>
+                    <div class="tooltip">
+                        Prenotato da:<br>
+                        <strong><?php echo htmlspecialchars($omb['nome'] . ' ' . $omb['cognome']); ?></strong><br>
+                        Dal: <?php echo htmlspecialchars($omb['data_inizio']); ?><br>
+                        Al: <?php echo htmlspecialchars($omb['data_fine']); ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php endfor; ?>
         </div>
-        <?php endforeach; ?>
+        <div class="fila fila-3">
+            <?php for ($i = 7; $i >= 6; $i--): $omb = $ombrelloni[10-$i];
+                $prenotato = !empty($omb['id_prenotazioni']);
+                $class = $prenotato ? 'prenotato' : 'libero';
+            ?>
+            <div class="ombrellone <?php echo $class; ?>" onclick="modificaOmbrellone(<?php echo $omb['id_ombrellone']; ?>)">
+                <?php echo $omb['n_ombrellone']; ?><br><small><?php echo $omb['fila']; ?></small>
+                <?php if ($prenotato): ?>
+                    <div class="tooltip">
+                        Prenotato da:<br>
+                        <strong><?php echo htmlspecialchars($omb['nome'] . ' ' . $omb['cognome']); ?></strong><br>
+                        Dal: <?php echo htmlspecialchars($omb['data_inizio']); ?><br>
+                        Al: <?php echo htmlspecialchars($omb['data_fine']); ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php endfor; ?>
+        </div>
+        <div class="fila fila-2">
+            <?php for ($i = 5; $i >= 3; $i--): $omb = $ombrelloni[10-$i];
+                $prenotato = !empty($omb['id_prenotazioni']);
+                $class = $prenotato ? 'prenotato' : 'libero';
+            ?>
+            <div class="ombrellone <?php echo $class; ?>" onclick="modificaOmbrellone(<?php echo $omb['id_ombrellone']; ?>)">
+                <?php echo $omb['n_ombrellone']; ?><br><small><?php echo $omb['fila']; ?></small>
+                <?php if ($prenotato): ?>
+                    <div class="tooltip">
+                        Prenotato da:<br>
+                        <strong><?php echo htmlspecialchars($omb['nome'] . ' ' . $omb['cognome']); ?></strong><br>
+                        Dal: <?php echo htmlspecialchars($omb['data_inizio']); ?><br>
+                        Al: <?php echo htmlspecialchars($omb['data_fine']); ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php endfor; ?>
+        </div>
+        <div class="fila fila-1">
+            <?php for ($i = 2; $i >= 1; $i--): $omb = $ombrelloni[10-$i];
+                $prenotato = !empty($omb['id_prenotazioni']);
+                $class = $prenotato ? 'prenotato' : 'libero';
+            ?>
+            <div class="ombrellone <?php echo $class; ?>" onclick="modificaOmbrellone(<?php echo $omb['id_ombrellone']; ?>)">
+                <?php echo $omb['n_ombrellone']; ?><br><small><?php echo $omb['fila']; ?></small>
+                <?php if ($prenotato): ?>
+                    <div class="tooltip">
+                        Prenotato da:<br>
+                        <strong><?php echo htmlspecialchars($omb['nome'] . ' ' . $omb['cognome']); ?></strong><br>
+                        Dal: <?php echo htmlspecialchars($omb['data_inizio']); ?><br>
+                        Al: <?php echo htmlspecialchars($omb['data_fine']); ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <?php endfor; ?>
+        </div>
+        <div class="mare"></div>
     </div>
     <script>
         function modificaOmbrellone(id) {
